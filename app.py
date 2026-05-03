c93816e531b1d94eb63ed9578a56ef6bbfacec9c
Angel_Escobar <angel.daniel.escobar77@gmail.com>
2026-05-03 00:44:13 -0400

    Agregaciones: - BD.txt : indicaciones sobre la base de datos
    Modificaciones:
    - Se probo conexion con redis y cassandra (comprobar en consola al ejecutar)

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, request
+from cassandra.cluster import Cluster
+import redis
 
 app = Flask(__name__)
+
+
+# Conexion a Redis
+r = redis.Redis(
+    host='localhost',
+    port=6379,
+    decode_responses=True
+)
+print("Conectado a Redis")
+
+
+# Conexion a Cassandra
+cluster = Cluster(['127.0.0.1'], port=9042)
+session = cluster.connect('energia')
+print("Conectado a Cassandra")
+
 
 @app.route('/consumo', methods=['POST'])
 def registrar_consumo():
@@ -8,6 +26,11 @@ def registrar_consumo():
     data = request.json
 
     print(data)
+    
+    r.set(
+        "LUZ_AULA_1:potencia",
+        data["potencia"]
+    )
 
     return {
         "mensaje": "dato recibido"
